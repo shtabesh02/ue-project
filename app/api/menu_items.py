4d50f484c652eacbc432e1b7c1797ef9b44f7e91
shtabesh02 <shtabesh02@gmail.com>
2024-04-25 01:47:08 -0400

    menu-items features

--- app/api/menu_items.py
+++ app/api/menu_items.py
@@ -5,26 +5,30 @@ from ..forms import MenuItemForm
 
 menu_items_routes = Blueprint("menu-items", __name__)
 
+# Moved to restaurant_routes.py because the menu belongs to a restaurant
+# api: api/restaurant/<init:id>/menu-items 
 
-@menu_items_routes.route("")
-@login_required
-def show_menu_items():
-    menu_items = MenuItem.query.all()
+# @menu_items_routes.route("")
+# @login_required
+# def show_menu_items():
+#     menu_items = MenuItem.query.all()
 
-    return {"menu_items": [menu_item.to_dict() for menu_item in menu_items]}
+#     return {"menu_items": [menu_item.to_dict() for menu_item in menu_items]}
 
 
-@menu_items_routes.route("/<int:id>")
+@menu_items_routes.route("/<int:id>/")
 # @login_required
 def item_details(id):
     item_details = MenuItem.query.filter_by(id=id).first()
-
+    print('item details: ', item_details)
     return {
         "id": item_details.id,
+        "restaurant_id": item_details.restaurant_id,
+        "type": item_details.type,
         "food_name": item_details.food_name,
         "description": item_details.description,
         "price": item_details.price,
-        "image": item_details.img_url,
+        "img_url": item_details.img_url,
     }
 
 
@@ -32,15 +36,14 @@ def item_details(id):
 @menu_items_routes.route("/<int:id>", methods=["PUT"])
 @login_required
 def update_items(id):
+    menu_item = MenuItem.query.get(id)
     form = MenuItemForm()
     form["csrf_token"].data = request.cookies["csrf_token"]
 
     if form.validate_on_submit():
-        menu_item = MenuItem.query.get(id)
-
-        if int(current_user.get_id()) != menu_item.restaurant.user_id:
-            return {"errors": {"message": "Unauthorized"}}, 401
-
+        # if int(current_user.get_id()) != menu_item.restaurant.user_id:
+        #     return {"errors": {"message": "Unauthorized"}}, 401
+        menu_item.type = form.data["type"]
         menu_item.food_name = form.data["food_name"]
         menu_item.decription = form.data["description"]
         menu_item.price = form.data["price"]
@@ -51,17 +54,19 @@ def update_items(id):
     return form.errors
 
 
-@menu_items_routes.route("/<int:id>", methods=["DELETE"])
-@login_required
-def delete_item(id):
-    menu_item = MenuItem.query.get(id)
+# Moved to restaurant_routes.py
 
-    if not menu_item:
-        return {"message": "Menu Item couldn't be found"}
+# @menu_items_routes.route("/<int:id>", methods=["DELETE"])
+# @login_required
+# def delete_item(id):
+#     menu_item = MenuItem.query.get(id)
 
-    if int(current_user.get_id()) != menu_item.restaurant.user_id:
-        return {"errors": {"message": "Unauthorized"}}, 401
+#     if not menu_item:
+#         return {"message": "Menu Item couldn't be found"}
 
-    db.session.delete(menu_item)
-    db.session.commit()
-    return {"message": "Successfully deleted Menu Item"}
+#     if int(current_user.get_id()) != menu_item.restaurant.user_id:
+#         return {"errors": {"message": "Unauthorized"}}, 401
+
+#     db.session.delete(menu_item)
+#     db.session.commit()
+#     return {"message": "Successfully deleted Menu Item"}
--- app/forms/menu_items_form.py
+++ app/forms/menu_items_form.py
@@ -1,8 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import IntegerField, StringField, TextAreaField, SubmitField
+from wtforms import IntegerField, StringField, TextAreaField
 from wtforms.validators import DataRequired
 
 class MenuItemForm(FlaskForm):
+    type = StringField('Type', validators=[DataRequired()])
     food_name = StringField('Food Name', validators=[DataRequired()])
     description = TextAreaField('Description', validators=[DataRequired()])
     price = IntegerField('Price', validators=[DataRequired()])
