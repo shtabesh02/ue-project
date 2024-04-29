c1a6cbf44e32e6d56ae403980d0699828ae50884
Chris W <97860788+AGiggleSniffer@users.noreply.github.com>
2024-04-28 22:53:59 -0700

    backend user validation

--- app/api/restaurant_routes.py
+++ app/api/restaurant_routes.py
@@ -125,8 +125,8 @@ def delete_item(item_id):
     if not menu_item:
         return {"message": "Menu Item couldn't be found"}
 
-    # if int(current_user.get_id()) != menu_item.restaurant.user_id:
-    #     return {"errors": {"message": "Unauthorized"}}, 401
+    if int(current_user.get_id()) != menu_item.restaurant.user_id:
+        return {"errors": {"message": "Unauthorized"}}, 401
 
     db.session.delete(menu_item)
     db.session.commit()
