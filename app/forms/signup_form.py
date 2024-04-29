4a4180c98308e555c3274abfe5397b063d5b895d
shtabesh02 <shtabesh02@gmail.com>
2024-04-29 18:08:27 -0400

    logs removed

--- app/forms/signup_form.py
+++ app/forms/signup_form.py
@@ -19,16 +19,42 @@ def username_exists(form, field):
     if user:
         raise ValidationError('Username is already in use.')
 
+# bellow validations are for signup
+def first_name(form, field):
+    if not field.data.isalpha() or len(field.data) > 40:
+        raise ValidationError("First name must be only alphabets, and less than 40 characters.")
+def last_name(form, field):
+    if not field.data.isalpha() or len(field.data) > 40:
+        raise ValidationError("Last name must be only alphabets, and less than 40 characters.")
+def password(form, field):
+    if len(field.data) < 4:
+        raise ValidationError("Password can not be less than 4 characters. It should be more than 4 characters")
+def email(form, field):
+    if not '@' in field.data or not '.' in field.data or len(field.data) > 255:
+        raise ValidationError("Invalid email format. Also it should be less than 255 characters")
+def address(form, field):
+    if len(field.data) > 100:
+        raise ValidationError("Address must be less than 100 characters")
+def city(form, field):
+    if len(field.data) > 40:
+        raise ValidationError("City name must be less than 40 characters")
+def country(form, field):
+    if len(field.data) > 100:
+        raise ValidationError("Country name must be less than 40 characters")
+def phone(form, field):
+    if not field.data.isdigit() or len(field.data) != 10:
+        raise ValidationError("Phone number must be 10 digits.")
+# above validations are for signup
 
 class SignUpForm(FlaskForm):
     username = StringField(
         'username', validators=[DataRequired(), username_exists])
-    email = StringField("email", validators=[DataRequired(), user_exists])
-    password = StringField('password', validators=[DataRequired()])
-    first_name = StringField("first_name", validators=[DataRequired()])
-    last_name = StringField("last_name", validators=[DataRequired()])
-    email = StringField("email", validators=[DataRequired()])
-    address = StringField("address", validators=[DataRequired()])
-    city = StringField("city", validators=[DataRequired()])
-    country = StringField("country", validators=[DataRequired()])
-    phone = StringField("phone", validators=[DataRequired()])
+    email = StringField("email", validators=[DataRequired(), user_exists, email])
+    password = StringField('password', validators=[DataRequired(), password])
+    first_name = StringField("first_name", validators=[DataRequired(), first_name])
+    last_name = StringField("last_name", validators=[DataRequired(), last_name])
+    # email = StringField("email", validators=[DataRequired(), email])
+    address = StringField("address", validators=[DataRequired(), address])
+    city = StringField("city", validators=[DataRequired(), city])
+    country = StringField("country", validators=[DataRequired(), country])
+    phone = StringField("phone", validators=[DataRequired(), phone])
