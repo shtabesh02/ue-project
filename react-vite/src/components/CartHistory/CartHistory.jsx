91c38f81bff949c533f6444deb7a3a3c36e46907
Chris W <97860788+AGiggleSniffer@users.noreply.github.com>
2024-06-23 10:24:48 -0700

    reorder and history done

--- react-vite/src/components/CartHistory/CartHistory.jsx
+++ react-vite/src/components/CartHistory/CartHistory.jsx
@@ -1,11 +1,25 @@
 import { useDispatch, useSelector } from "react-redux";
-import { historyArr, thunkGetHistory } from "../../redux/cart";
+import { useNavigate } from "react-router-dom";
+import { historyArr, thunkGetHistory, addCartItem } from "../../redux/cart";
 import { useEffect } from "react";
 import "./CartHistory.css";
 
 export default function CartHistory() {
-	const dispatch = useDispatch();
+	const stateRestId = useSelector((state) => state.cart.restaurantId);
 	const history = useSelector(historyArr);
+	const dispatch = useDispatch();
+	const navigate = useNavigate();
+
+	const handleReorder = (id) => {
+		const { menu_items } = history.find(([i]) => i === id)[1];
+		if (stateRestId && stateRestId !== menu_items[0].restaurant_id) {
+			return alert(
+				"To add items from a different restaurant, delete items currently in your cart.",
+			);
+		}
+		menu_items.forEach((item) => dispatch(addCartItem(item)));
+		return navigate(`/cart`);
+	};
 
 	useEffect(() => {
 		dispatch(thunkGetHistory());
@@ -13,8 +27,7 @@ export default function CartHistory() {
 
 	return (
 		<div className="CartHistory">
-			{history?.map(([id, receipt]) => {
-				const { time, menu_items } = receipt;
+			{history?.map(([id, { time, menu_items }]) => {
 				const price = menu_items.reduce((acc, val) => acc + val.price, 0);
 				const s = menu_items.length < 2 ? "" : "s";
 				const localTime = new Date(`${time} UTC`);
@@ -24,14 +37,21 @@ export default function CartHistory() {
 
 				return (
 					<div key={id}>
-						<h3>
+						<h3
+							onClick={() =>
+								navigate(`/restaurants/${menu_items[0].restaurant_id}`)
+							}
+						>
 							<span>{menu_items[0].restaurant_name}</span> <span>{">"}</span>
 						</h3>
 						<p>
-							{localTime.toDateString()} - {menu_items.length} item{s} - $
-							{price} (subtotal)
+							<span>
+								{localTime.toDateString()} - {menu_items.length} item{s} - $
+								{price} (subtotal)
+							</span>
 						</p>
 						<p>{menuString}</p>
+						<span onClick={() => handleReorder(id)}>Reorder?</span>
 					</div>
 				);
 			})}
